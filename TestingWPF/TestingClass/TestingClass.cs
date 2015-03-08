2498973ec6bd4e9fef144a4b69d001fc3deada4f
IngJavierR <hazelapd@gmail.com>
2015-03-07 22:47:20 -0600

    Routing Events WPF

---
+++ TestingWPF/TestingClass/TestingClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TestingWPF;
+
+namespace TestingClass
+{
+    [TestFixture]
+    class TestingClass
+    {
+        [Test]
+        public void testFormatName(){
+            var test = new MainWindow();
+            Assert.AreEqual("PRUEBA", test.obtieneTextoFormateado("prueba"));
+        }
+        
+    }
+}
